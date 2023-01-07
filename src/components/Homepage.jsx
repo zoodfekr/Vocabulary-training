217fb08f2c74e3db39da4860aa6abdb120f545fe
ramin <zoodfekr.r@gmail.com>
2023-01-07 16:18:16 +0330

    input

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../style/style.scss'
 
 import { Portal } from 'react-portal';
 
@@ -6,12 +7,27 @@ const Home = () => {
 
 	return (
 
-		<div className='container border d-flex justify-content-center p-3 mt-2 flex-column align-items-center'>
+		<>
+			<div className='container border bg-light d-flex flex-row flex-wrap d-flex justify-content-center p-2' >
 
 
+				<div className=' d-flex justify-content-center'>
 
-		</div>
 
+					<form action="" className=' p-2' style={{ borderRadius: "25px" }}>
+
+						<input type="text" className='form-control rtl my-1 input' placeholder='لغت خود را وارد نمائید' />
+						<input type="text" className='form-control my-1 input' placeholder='معنی' />
+
+						<button type='submit' className='btn btn-success w-100'>ثبت</button>
+					</form>
+
+				</div>
+
+			</div>
+
+
+		</>
 	)
 }
 
