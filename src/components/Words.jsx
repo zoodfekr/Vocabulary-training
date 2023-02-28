b4fc63df6e19a7ac76e47e99b66fb1920d1b23f7
zoodfekr <zoodfekr.r@gmail.com>
2023-02-28 13:25:09 +0330

    test

--- src/components/Words.jsx
+++ src/components/Words.jsx
@@ -1,12 +1,23 @@
 import React, { useEffect, useState, useRef, useContext, Suspense } from 'react';
 import Word from './Word';
 import AppContext from '../context/Context';
+import { getCities } from '../services/firebaseService';
 
 const Words = () => {
 	const { datawords, costomcolor, theme } = useContext(AppContext);
+
+	const [cities, setCities] = useState(null);
+
+	useEffect(() => {
+	  getCities().then((firebaseCities) => {
+		setCities(firebaseCities);
+	  });
+	}, []);
+
 	return (
 
 		<div className="words">
+			{ cities != null ? cities.map(c => <small>{c.name}</small>) : <p>Loading</p>}
 			<ol className=" justify-content-evenly d-flex flex-wrap p-0 ">
 				{datawords?.map(datawords => {
 					return (
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBKpkhYSBXsn4JbH6w1WTcA5p6Dg9wxTZQ",
+    authDomain: "vocabulary-training-f6bdc.firebaseapp.com",
+    projectId: "vocabulary-training-f6bdc",
+    storageBucket: "vocabulary-training-f6bdc.appspot.com",
+    messagingSenderId: "762391712431",
+    appId: "1:762391712431:web:a45b13a2c8c28d4fd23a5a",
+    measurementId: "G-XXQR81CYSK"
+};
